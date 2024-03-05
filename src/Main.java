7d78848a3f788f41fc226efaa5c2e8f572aacdbc
Lahoucine Chikry <socialhcdia@gmail.com>
2024-03-05 12:51:06 +0000

    edit main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Worldddd!");
-        System.out.println("have you seen this!");
     }
 }

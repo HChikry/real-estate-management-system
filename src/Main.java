701f5e4a3ec05fed8610481142b3374dfd715612
Lahoucine Chikry <socialhcdia@gmail.com>
2024-03-05 12:45:16 +0000

    rename and modify App.java to Main.java

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Worldddd!");
+        System.out.println("have you seen this!");
+    }
+}
